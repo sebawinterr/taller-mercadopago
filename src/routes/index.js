536eefe0992a6a99e54c189e3e6c4c8555d63094
sebawinterr <wintersebast@gmail.com>
2020-12-08 22:24:41 -0300

    Webhooks

--- src/routes/index.js
+++ src/routes/index.js
@@ -12,5 +12,9 @@ router.get('/detail', indexController.detail);
 router.post('/comprar', indexController.comprar);
 router.get('/callback', indexController.callback);
 router.post('/notifications', indexController.notifications);
+router.post('/webhoooks', function(req, res) {
+    console.log('webhook', req.body);
+    res.send(req.body);
+})
 
 module.exports = router;
